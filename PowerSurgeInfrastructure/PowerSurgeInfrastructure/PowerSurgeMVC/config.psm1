381b49388d8b957da805dea6a0226b8c0e825c74
Steve Rathbone <steve@steve555.info>
2017-11-04 23:23:09 +1100

    more minor fixes

--- PowerSurgeInfrastructure/PowerSurgeInfrastructure/PowerSurgeMVC/config.psm1
+++ PowerSurgeInfrastructure/PowerSurgeInfrastructure/PowerSurgeMVC/config.psm1
@@ -1,19 +1,14 @@
-#Application specific settings only!
+
 Set-Variable -Name 'logging' -Value $false -Option Constant;
 #uncomment the following line if you wish to have DB support
  #. $PowerSurgeAppPath\core\DB.ps1
 
-$global:steve = 31;
-[string]$debugString = 'Debug Info: ';
+[string] $debugString = 'Debug Info: ';
 
-$global:AppConfig = @{
+$Global:AppConfig = @{
     'ApplicationName' = 'PowerSurge MVC';
     'Logging' = $false;
     'Debugging' = $false;
     'DBConnectionString' = '';
     'UseGlobalView' = $true;
-    'defaultRoute' ='/Home/Index'
 }
-
-
-
--- PowerSurgeInfrastructure/PowerSurgeInfrastructure/PowerSurgeMVC/routes.ps1
+++ PowerSurgeInfrastructure/PowerSurgeInfrastructure/PowerSurgeMVC/routes.ps1
@@ -2,9 +2,11 @@ function Get-Routes {
 	[CmdletBinding()]
 	[OutputType([System.Collections.Generic.List[System.string[]]])]
 	$routes = New-Object System.Collections.Generic.List[System.string[]];
-	$null = $routes.Add( 
-		@('^$', 'Home', 'Index'));
-	$null = $routes.Add( @('^About$', 'Home', 'About'));
 	
-	$routes
+	$routes.Add( @('^$', 'Home', 'Index'));
+	$routes.Add( @('^About$', 'Home', 'About'));
+	
+	## Define new routes here ##
+	
+	$routes; 
 }
